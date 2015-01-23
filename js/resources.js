d5740f13c1c9174b91560f89651489c2e711a58a
kenice washington <kenicek56@gmail.com>
2015-01-23 12:12:02 -0800

    finished video 4

--- js/resources.js
+++ js/resources.js
@@ -4,12 +4,13 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
-
+     {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+     {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
 	 */
-		
+	 {name: "level01", type: "tmx", src: "data/map/test.tmx"},
 	/* Maps. 
 	 * @example
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
