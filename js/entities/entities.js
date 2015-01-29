a7188f9b5123fe14b64b140d0d1e4bb8b105b109
kenice washington <kenicek56@gmail.com>
2015-01-28 18:38:44 -0800

    fixed some errors

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,12 +1,12 @@
 game.PlayerEntity = me.Entity.extend({
   
-    init:function(x, y, settings){
-        this._super(me.Entity, 'init', [x, y, {
-        image:"player",
-        width: 64,
-        height: 64,
-        spritewidth: "64",
-        spriteheight: "64",
+	init:function(x, y, settings){
+		this._super(me.Entity, 'init', [x, y, {
+		image:"player",
+		width: 64,
+		height: 64,
+		spritewidth: "64",
+		spriteheight: "64",
 
 getShape: function() {
 return (new me.Rect(0, 0, 64, 64)).toPolygon();
@@ -14,24 +14,24 @@ return (new me.Rect(0, 0, 64, 64)).toPolygon();
 }
 
 }]);
-
+// changes my players y vaule to where he spawns
 this.body.setVelocity(5, 20);
-    },
+	},
 
-    update:function(delta) {
-    	if(me.input.isKeyPressed("right")) {
-// sets the position of my x by adding the velocity defined above in
+	update:function(delta) {
+		if(me.input.isKeyPressed("right")) {
+// adds the position of my x by adding the velocity defined above in
 //setVelocity() and mutiplying it by me.timer.tick
 // me.timer.tick makes the movement look smooth
-    		this.body.vel.x += this.body.accel.x * me.timer.tick;
-    	}
+			this.body.vel.x += this.body.accel.x * me.timer.tick;
+		}
 
-    	else{
-    		this.body.vel.x = 0;
-    	}
+		else{
+			this.body.vel.x = 0;
+		}
 
-    	this.body.update(delta);
-        return true;
-    }
+		this.body.update(delta);
+		return true;
+	}
 
 });
