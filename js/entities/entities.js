29e5c83ef991a9b4b925b8193303544edba44e4c
kenice washington <kenicek56@gmail.com>
2015-01-29 12:29:28 -0800

    awserdtghbnklm;,;

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -62,3 +62,28 @@ game.PlayerEntity = me.Entity.extend({
 		return true;
 	}
 });
+
+
+game.PlayerBaseEntity = me.Entity.extend ({
+	init : function(x, y, settings) {
+		this._super(me.Entity, 'init', [x, y, {
+             image: "tower",
+             width: 100,
+             height: 100,
+             spritewidth: "100",
+             spriteheight: "100",
+             getShape: function() {
+             	return (new me.Rect(0, 0, 100, 100)).toPolygon();
+             }
+		}]);
+		this.broken = false;
+		this.health = 10;
+		this.alwaysUpdate = true;
+		this.body.onCollision = this.onCollision.bind(this);
+	},
+
+	update:function() {
+
+	}
+
+});
