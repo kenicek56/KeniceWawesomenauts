e24af2e03dac6956ce4743bdf3eebea7289d8b46
kenice washington <kenicek56@gmail.com>
2015-03-30 00:58:47 -0700

    finished video 52!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- js/resources.js
+++ js/resources.js
@@ -14,6 +14,8 @@ game.resources = [
       {name: "title-screen", type:"image", src: "data/img/awesomenauts.jpg"},
       {name: "exp-screen", type:"image", src: "data/img/loadpic.png"},
       {name: "gold-screen", type:"image", src: "data/img/spend.png"},
+      {name: "load-screen", type:"image", src: "data/img/loadpic.png"},
+	  {name: "new-screen", type:"image", src: "data/img/newpic.png"},
 
 	/* Atlases 
 	 * @example
---
+++ js/screens/loadProfile.js
@@ -0,0 +1,38 @@
+game.LoadProfile = me.ScreenObject.extend({
+	/**	
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {	
+		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('load-screen')), -10); // TODO
+		me.audio.playTrack("Awesomenauts-upgradeTune");
+		
+		me.input.unbindKey(me.input.KEY.B);
+		me.input.unbindKey(me.input.KEY.Q);
+		me.input.unbindKey(me.input.KEY.E);
+		me.input.unbindKey(me.input.KEY.W);
+		me.input.unbindKey(me.input.KEY.A);
+
+		me.game.world.addChild(new (me.Renderable.extend({
+			init: function() {
+				this._super(me.Renderable, 'init', [10, 10, 300, 50]);
+				/*the settings of the text*/
+				this.font = new me.Font("Arial", 26, "gold");		
+			},
+
+			/*Adds the text and sets the coordinates of the text*/
+			draw: function(renderer) {
+				this.font.draw(renderer.getContext(), "ENTER YOUR USERNAME AND PASSWORD", this.pos.x, this.pos.y);
+			}
+
+		})));
+
+	},
+	
+	
+	/**	
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+		
+	}	
+});
