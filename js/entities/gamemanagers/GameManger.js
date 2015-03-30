cd5ad65d071bbc0d6cd238d69b789d9b18c0e442
kenice washington <kenicek56@gmail.com>
2015-03-30 00:45:50 -0700

    advanced: finished video 51!

---
+++ js/entities/gamemanagers/GameManger.js
@@ -0,0 +1,30 @@
+game.ExperienceManager = Object.extend({
+    init: function(x, y, settings){
+        this.alwaysUpdate = true;
+        game.gameover = false;
+
+    },
+    update: function(){
+        if(game.data.win === true && !this.gameover){
+            this.gameOver(true);
+        }else if(game.data.win === false && !this.gameover){
+            this.gameOver(false);
+            
+        }
+
+        return true;
+    },
+
+    gameOver: function(win){
+        if(win){
+            game.data.exp += 10;
+        }else{
+            game.data.exp += 1;
+        }
+            
+            game.gameover = true;
+            me.save.exp = game.data.exp;
+            
+    }
+});
+
--- js/entities/gamemanagers/SpendGold.js
+++ js/entities/gamemanagers/SpendGold.js
@@ -1,81 +1,3 @@
-//puts the enemy on  a timer
-game.GameTimerManager = Object.extend({
-init: function(x, y, settings){
-this.now = new Date().getTime();
-this.lastCreep = new Date().getTime();
-this.paused = false;
-this.alwaysUpdate = true;
-},
-update: function(){
-    //keeps track of time
-this.now = new Date().getTime();
-
-this.goldTimerCheck();
-this.creepTimerCheck();
-
-
-    return true;
-},
-goldTimerCheck: function(){
-    if(Math.round(this.now/1000)%20 ===0 && (this.now - this.lastCreep >= 1000)){
-    game.data.gold += (game.data.exp1+1);
-    console.log("Current gold:" + game.data.gold);
-        }
-
-    },
-    creepTimerCheck: function(){
-        if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)){
-    this.lastCreep = this.now;
-    var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
-    me.game.world.addChild(creepe, 5);
-        }
-    }
-});
-
-game.HeroDeathManager = Object.extend({
-    init: function(x, y, settings){
-        this.alwaysUpdate = true;
-    },
-    update: function(){
-        if(game.data.player.dead){
-    me.game.world.removeChild(game.data.player);
-    //reseting the player
-    me.state.current().resetPlayer(10, 0);
-        }
-        return true;
-    }
-});
-
-game.ExperienceManager = Object.extend({
-    init: function(x, y, settings){
-        this.alwaysUpdate = true;
-        game.gameover = false;
-
-    },
-    update: function(){
-        if(game.data.win === true && !this.gameover){
-            this.gameOver(true);
-        }else if(game.data.win === false && !this.gameover){
-            this.gameOver(false);
-            
-        }
-
-        return true;
-    },
-
-    gameOver: function(win){
-        if(win){
-            game.data.exp += 10;
-        }else{
-            game.data.exp += 1;
-        }
-            
-            game.gameover = true;
-            me.save.exp = game.data.exp;
-            
-    }
-});
-
 game.SpendGold = Object.extend ({
     init: function(x, y, settings) {
        this.now = new Date().getTime();
@@ -205,7 +127,7 @@ game.data.buytext = new (me.Renderable.extend({
        if(skill === 1){
            game.data.gold -= ((game.data.skill1 +1)* 10);
            game.data.skill1 += 1;
-           game.data.player.attack += 1;
+           game.data.playerAttack += 1;
        }else if(skill ===2){
            game.data.gold -= ((game.data.skill2 +1)* 10);
            game.data.skill2 += 1;
