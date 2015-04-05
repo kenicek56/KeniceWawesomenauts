e75839ed8758f9e118ee5cd1a2cc6406877bfcfd
kenice washington <kenicek56@gmail.com>
2015-04-05 15:20:37 -0700

    intermediate code: i made an enemy hero

--- js/resources.js
+++ js/resources.js
@@ -16,6 +16,8 @@ game.resources = [
       {name: "gold-screen", type:"image", src: "data/img/spend.png"},
       {name: "load-screen", type:"image", src: "data/img/loadpic.png"},
 	  {name: "new-screen", type:"image", src: "data/img/newpic.png"},
+	  {name: "hero", type:"image", src: "data/img/skeletonDagger.png"},
+
 
 	/* Atlases 
 	 * @example
