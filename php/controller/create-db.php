9f1b2dbccceffadbc1ec1dd0a2c38fa246f8d452
kenice washington <kenicek56@gmail.com>
2015-04-05 15:40:22 -0700

    advanced: finished video 55 finishing PHP

--- php/controller/create-db.php
+++ php/controller/create-db.php
@@ -2,29 +2,17 @@
 // access my model
 require_once(__DIR__ . "/../model/config.php");
 
-
-// creates a blog post
-$query = $_SESSION["connection"]->query("CREATE TABLE posts (" 
-	// null indicates that when ever we create a new blog post the id cant be known and id cant be empty.
-	. "id int (11) NOT NULL AUTO_INCREMENT,"
-	. "title varchar (255) NOT NULL,"
-	. "post text NOT NULL,"
-	. "PRIMARY KEY (id))");
-// 1 means there are 11 values.
-// tells us if or query is running or not
-if($query) {
-	//echo "<p>Successfully created table: posts</p>";
-}
-else {
-	echo "<p>" . $_SESSION["connection"]->error . "</p>";
-}
-
 $query = $_SESSION["connection"]->query("CREATE TABLE users ("
 	. "id int(11) NOT NULL AUTO_INCREMENT,"
 	. "username varchar(30) NOT NULL,"
 	. "email varchar(50) NOT NULL,"
 	. "password char(128) NOT NULL,"
 	. "salt char(128) NOT NULL,"
+	. "exp int(4),"
+	. "exp1 int(4),"
+	. "exp2 int(4),"
+	. "exp3 int(4),"
+	. "exp4 int(4),"
 	. "PRIMARY KEY (id))");
 
 if($query) {
