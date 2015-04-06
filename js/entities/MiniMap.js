3a070c991f14404481b55cd7ca762145fa3ce210
kenice washington <kenicek56@gmail.com>
2015-04-05 22:36:44 -0700

    intermediate: finished video 64/6 displaying my mini map

--- js/entities/MiniMap.js
+++ js/entities/MiniMap.js
@@ -1,3 +1,16 @@
 game.MiniMap = me.Entity.extend ({
+init: function(x, y, settings) {
+	this._super(me.Entity, "init", [x, y, {
+        image: "minimap"
+        width: 844
+        height: 119
+        spritewidth:"844"
+        spriteheight:"119"
+        getShape: function() {
+        	return (new me.Rect(0, 0, 844, 119)).toPolygon();
+        }
+	 }]);
+  this.floating = true;
 
+   }
 });
